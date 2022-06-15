84eb5cdbc8c7d207c10d6c939d8441e884454555
Andrew Cowan <100159853+agcowan@users.noreply.github.com>
2022-06-14 20:44:16 -0400

    Completed jupyter notebook, converted to .py file, finished creating flask app, and index.html.

---
+++ Mission_to_Mars/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+# Instance of Flask app
+app = Flask(__name__)
+
+# Use flask_pymongo to set up connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# Home route
+@app.route("/")
+def index():
+    mars_info = mongo.db.collection.find_one()
+
+    # Return template
+    return render_template("index.html", space = mars_info)
+
+@app.route("/scrape")
+def scraper():
+    
+    # Run the scrape function
+    mars_data = scrape_mars.scrape_info()
+
+    # Update Mongo database using update and upsert
+    mongo.db.collection.update_one({}, {"$set":mars_data}, upsert=True)
+
+    # Redirect back to home
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Mission_to_Mars/scrape_mars.py
@@ -0,0 +1,118 @@
+# Dependencies
+from splinter import Browser
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+
+def scrape_info():
+    # Automatic browser
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome',**executable_path, headless=False)
+
+
+    # Visit news site https://redplanetscience.com
+    news_url = "https://redplanetscience.com/"
+    browser.visit(news_url)
+
+    # Parser
+    html = browser.html
+    soup = BeautifulSoup(html,'html.parser')
+
+    # Find the first article div, class list_text
+    article = soup.find('div', class_='list_text')
+
+    # Scrape the title and save in a variable
+    title = article.find('div', class_='content_title').text
+
+    # Scrape the paragraph text and save in a variable
+    paragraph = article.find('div', class_='article_teaser_body').text
+
+    # Visit Featured Space Image site https://spaceimages-mars.com
+    img_url = "https://spaceimages-mars.com/"
+    browser.visit(img_url)
+
+    # Parser
+    html = browser.html
+    soup = BeautifulSoup(html,'html.parser')
+
+    # Find the div with the Featured Mars Image
+    header = soup.find('div',class_='header')
+
+    # Extract the URL from the header used for the Featured Image
+    image = header.find('img',class_ = 'headerimage fade-in')
+    featured_image_url = img_url + image['src']
+
+    # Visit Mars Facts site https://galaxyfacts-mars.com
+    facts_url = "https://galaxyfacts-mars.com/"
+    browser.visit(facts_url)
+
+    # Use Pandas to scrape the planet profile table
+    table = pd.read_html(facts_url)
+
+    # Convert the scraped table to a DataFrame
+    df = table[1]
+
+    # Convert DataFrame to table, replace values with blanks 
+    html_table = df.to_html().replace('\n', '')
+
+    # Save the file as .html
+    df.to_html('table.html')
+
+    # Visit astrogeology site
+    hemi_url = "https://marshemispheres.com/"
+    browser.visit(hemi_url)
+
+    # Empty list for the dictionaries
+    hemisphere_image_urls = []
+    titles = []
+
+    # Parser
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Click on links to go to the hemisphere pages
+    links = []
+    area = soup.find_all('a', class_ = 'itemLink')
+
+    for each in area:
+        try:
+            link = each.get('href')
+            if link not in links:
+                links.append(link)
+            browser.visit(hemi_url + link)
+            # Parser
+            html2 = browser.html
+            soup2 = BeautifulSoup(html2, 'html.parser')
+            # Scrape links to images
+            downloads = soup2.find('div', class_ = 'downloads')
+            anchor = downloads.a
+            href = anchor.get('href')
+            # Scrape h2 tags to get titles
+            title = soup2.find('div', class_ = 'cover')
+            name = title.h2.text
+            # If image link not in the list, append
+            if hemi_url + href not in hemisphere_image_urls:
+                hemisphere_image_urls.append(hemi_url + href)
+            # If title not in the list, append
+            if name not in titles:
+                titles.append(name)
+        except:
+            pass
+
+    browser.quit()
+
+    image_dict = {
+        "title":title[0],"image_url":hemisphere_image_urls[0],
+        "title":title[1],"image_url":hemisphere_image_urls[1],
+        "title":title[2],"image_url":hemisphere_image_urls[2],
+        "title":title[3],"image_url":hemisphere_image_urls[3],
+    }
+
+
+    mars_data = {
+        "image":featured_image_url,
+        "art_title":title,
+        "paragraph":paragraph
+    }
+
+    return mars_data, image_dict, html_table
